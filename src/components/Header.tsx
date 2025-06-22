68f2d29000da5fb9fa3eb28c35852385820f784c
Bukola A <bumolaya@gmail.com>
2025-06-21 18:44:02 -0700

    added logo and favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import bee from '../assets/bee.svg';
 import { ModeToggle } from './Theme-toggle';
 import CitySearch from './CitySearch';
 
@@ -11,8 +11,13 @@ const Header = () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background p-6 shadow-md supports-[backdrop-filter]:bg-background/40 backdrop-blur  border-amber-400">
       <div className="container mx-auto flex  items-center justify-between">
-        <Link to="/">
-          <h1>WeatherBee</h1>
+        <Link to="/" className="flex gap-1">
+          <img src={bee} alt="" width={24} height={24} />
+          <h1>
+            Weather
+            <span className="text-yellow-400">B</span>
+            ee
+          </h1>
         </Link>
         <div className="flex items-center gap-2 lg:gap-4">
           {/* search */}
